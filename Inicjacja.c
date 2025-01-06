0b855776b3192d6e43d978a0303188a9a01e6ece
AtelierRq <161210505+AtelierRq@users.noreply.github.com>
2025-01-06 18:33:24 +0100

    Create Inicjacja.c

---
+++ Inicjacja.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#define QUEUE_KEY 947270
+
+int main() {
+    int msgid = msgget(QUEUE_KEY, 0666 | IPC_CREAT);
+    if (msgid == -1) {
+        perror("msgget");
+        exit(1);
+    }
+
+    printf("Inicjator: Kolejka komunikatow utworzona. ID: %d\n", msgid);
+    return 0;
+}
