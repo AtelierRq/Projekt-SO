1e7c1b24492c54396dd84a213ecb9aad69412e77
AtelierRq <161210505+AtelierRq@users.noreply.github.com>
2025-01-12 13:51:32 +0100

    Update kierownik.c

--- kierownik.c
+++ kierownik.c
@@ -8,34 +8,50 @@
 
 typedef struct {
     long type;
-    int command;
+    int command;   // 1 = wstrzymanie, 2 = wpuszczanie, 3 = opuszczanie
+    int team_id;   // ID druzyny kibica
 } Message;
 
-void send_signal(int msgid, int command) {
+// Funkcja do wysylania sygnalu
+void send_signal(int msgid, int command, int team_id) {
     Message msg;
-    msg.type = 1;
-    msg.command = command;
-    msgsnd(msgid, &msg, sizeof(msg.command), 0);
-    printf("Kierownik wysyla sygnal: %d\n", command);
+    msg.type = 1;          // Typ wiadomosci dla pracownika
+    msg.command = command; // Komenda
+    msg.team_id = team_id; // ID druzyny (0 dla sygnalow bez druzyny)
+
+    msgsnd(msgid, &msg, sizeof(msg.command) + sizeof(msg.team_id), 0);
+    if (command == 1) {
+        printf("Kierownik wysyla sygnal: Wstrzymanie wpuszczania kibicow.\n");
+    } else if (command == 2) {
+        printf("Kierownik wysyla sygnal: Wpuszczanie kibicow z druzyny %d.\n", team_id);
+    } else if (command == 3) {
+        printf("Kierownik wysyla sygnal: Opuszczanie stadionu.\n");
+    }
 }
 
 int main() {
+    // Uzyskanie dostepu do kolejki komunikatow
     int msgid = msgget(QUEUE_KEY, 0666);
     if (msgid == -1) {
-        perror("msgget");
+        perror("Blad otwierania kolejki komunikatow");
         exit(1);
     }
 
-    printf("Kierownik: Wysylam sygnaly...\n");
-    send_signal(msgid, 1); // sygnal 1 - wstrzymanie wpuszczania
-    sleep(5);
+    printf("Kierownik: Maksymalna liczba kibicow na stadionie: 10000.\n");
+    printf("Kierownik: Rozpoczynam wysylanie sygnalow...\n");
 
-    send_signal(msgid, 2); // sygnal 2 - wznowienie wpuszczania
-    sleep(5);
+    // Symulacja wysylania sygnalow
+    send_signal(msgid, 1, 0); // Sygnal 1: Wstrzymanie
+    sleep(3);
 
-    send_signal(msgid, 3); // sygnal 3 - wyjscie kibicow
-    sleep(5);
+    send_signal(msgid, 2, 1); // Sygnal 2: Wpuszczanie druzyny 1
+    sleep(3);
 
+    send_signal(msgid, 2, 2); // Sygnal 3: Wpuszczanie druzyny 2
+    sleep(3);
+
+    send_signal(msgid, 3, 0); // Sygnal 3: Opuszczanie stadionu
     printf("Kierownik: Zakonczono wysylanie sygnalow.\n");
+
     return 0;
 }
