cf04a00607b0db15e53f88b88848daf05496bbac
AtelierRq <161210505+AtelierRq@users.noreply.github.com>
2025-01-12 13:49:00 +0100

    Update Inicjacja.c

--- Inicjacja.c
+++ Inicjacja.c
@@ -2,16 +2,40 @@
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
 
 #define QUEUE_KEY 947270
+#define SHM_KEY 24125
+#define SEM_KEY 91011
 
 int main() {
+    // Tworzenie kolejki komunikatow
     int msgid = msgget(QUEUE_KEY, 0666 | IPC_CREAT);
     if (msgid == -1) {
-        perror("msgget");
+        perror("Blad tworzenia kolejki komunikatow");
         exit(1);
     }
-
     printf("Inicjator: Kolejka komunikatow utworzona. ID: %d\n", msgid);
+
+    // Tworzenie pamieci wspoldzielonej dla stanowisk
+    int shmid = shmget(SHM_KEY, 3 * sizeof(int), 0666 | IPC_CREAT);
+    if (shmid == -1) {
+        perror("Blad tworzenia pamieci wspoldzielonej");
+        exit(1);
+    }
+    printf("Inicjator: Pamiec wspoldzielona utworzona. ID: %d\n", shmid);
+
+    // Tworzenie semaforow dla stanowisk
+    int semid = semget(SEM_KEY, 3, 0666 | IPC_CREAT);
+    if (semid == -1) {
+        perror("Blad tworzenia semaforow");
+        exit(1);
+    }
+    for (int i = 0; i < 3; i++) {
+        semctl(semid, i, SETVAL, 3); // Na kazdym stanowisku max 3 osoby
+    }
+    printf("Inicjator: Semafory utworzone.\n");
+
     return 0;
 }
