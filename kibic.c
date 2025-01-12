5855fd6048dbaea57bacfdda56deb8ed12145bb6
AtelierRq <161210505+AtelierRq@users.noreply.github.com>
2025-01-12 13:50:42 +0100

    Update kibic.c

--- kibic.c
+++ kibic.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <unistd.h>
 
 #define QUEUE_KEY 947270
 
 typedef struct {
     long type;
-    int command;
+    int command;  // 2 = wejscie, 3 = opuszczenie stadionu
+    int team_id;  // ID druzyny kibica
 } Message;
 
 int main() {
@@ -17,20 +19,27 @@ int main() {
         exit(1);
     }
 
-    Message msg;
-    while (1) {
-        // Odbieranie sygnalu od pracownika (type = 2)
-        if (msgrcv(msgid, &msg, sizeof(msg.command), 2, 0) == -1) {
-            perror("Blad odbioru wiadomosci w kibicu");
-            exit(1);
-        }
+    int my_team = rand() % 2 + 1; // Losowy numer druzyny
+    printf("Kibic z druzyny %d czeka na wejscie na stadion.\n", my_team);
 
-        if (msg.command == 2) {
-            printf("Kibic: Wchodze na stadion.\n");
-        } else if (msg.command == 3) {
-            printf("Kibic: Opuszczam stadion.\n");
-            break; // Kibic konczy dzialanie po sygnale 3
-        }
+    Message msg;
+    msg.type = 1;
+    msg.command = 2; // Proba wejscia
+    msg.team_id = my_team;
+
+    // Wyslanie proby wejscia
+    msgsnd(msgid, &msg, sizeof(msg.command) + sizeof(msg.team_id), 0);
+
+    // Oczekiwanie na odpowiedz
+    if (msgrcv(msgid, &msg, sizeof(msg.command) + sizeof(msg.team_id), 2, 0) == -1) {
+        perror("Blad odbioru wiadomosci");
+        exit(1);
+    }
+
+    if (msg.command == 2) {
+        printf("Kibic z druzyny %d wchodzi na stadion.\n", my_team);
+    } else if (msg.command == 3) {
+        printf("Kibic z druzyny %d opuszcza stadion.\n", my_team);
     }
 
     return 0;
