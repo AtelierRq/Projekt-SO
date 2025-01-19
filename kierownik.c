7eedc569265d86456babb2ee60b9064f8dec6f46
AtelierRq <161210505+AtelierRq@users.noreply.github.com>
2025-01-19 17:18:55 +0100

    Update kierownik.c

--- kierownik.c
+++ kierownik.c
@@ -4,39 +4,50 @@
 #include <sys/msg.h>
 #include <unistd.h>
 
-#define QUEUE_KEY 947270
+#define QUEUE_KEY 1234567
 
 typedef struct {
     long type;
-    int command;
+    int command; // 1 = wstrzymanie, 2 = wpuszczanie, 3 = opuszczanie
 } Message;
 
 void send_signal(int msgid, int command) {
     Message msg;
-    msg.type = 1;
+    msg.type = 1; // Typ wiadomości dla pracownika
     msg.command = command;
-    msgsnd(msgid, &msg, sizeof(msg.command), 0);
-    printf("Kierownik wysyla sygnal: %d\n", command);
+
+    if (msgsnd(msgid, &msg, sizeof(msg.command), 0) == -1) {
+        perror("Blad wysylania sygnalu");
+        exit(1);
+    }
+
+    if (command == 1) {
+        printf("Kierownik wysyla sygnal: Wstrzymanie wpuszczania kibicow.\n");
+    } else if (command == 2) {
+        printf("Kierownik wysyla sygnal: Wpuszczanie kibicow.\n");
+    } else if (command == 3) {
+        printf("Kierownik wysyla sygnal: Opuszczanie stadionu.\n");
+    }
 }
 
 int main() {
     int msgid = msgget(QUEUE_KEY, 0666);
     if (msgid == -1) {
-        perror("msgget");
+        perror("Blad otwierania kolejki komunikatow");
         exit(1);
     }
 
-    printf("Kierownik: Wysylam sygnaly...\n");
-    send_signal(msgid, 1); // sygnal 1 - wstrzymanie wpuszczania
-    sleep(5);
+    printf("Kierownik: Rozpoczynam wysylanie sygnalow...\n");
 
-    send_signal(msgid, 2); // sygnal 2 - wznowienie wpuszczania
-    sleep(5);
+    send_signal(msgid, 2); // Sygnał wpuszczania
+    sleep(3);
 
-    send_signal(msgid, 3); // sygnal 3 - wyjscie kibicow
-    sleep(5);
+    send_signal(msgid, 1); // Sygnał wstrzymania
+    sleep(3);
 
+    send_signal(msgid, 3); // Sygnał opuszczania
+    sleep(3);
     printf("Kierownik: Zakonczono wysylanie sygnalow.\n");
+
     return 0;
 }
-
