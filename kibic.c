f3dc884bcbf39022c97afe1ae617a33c6e507775
AtelierRq <161210505+AtelierRq@users.noreply.github.com>
2025-01-19 19:11:15 +0100

    Update kibic.c

--- kibic.c
+++ kibic.c
@@ -2,34 +2,55 @@
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <unistd.h>
+#include <time.h>
 
-#define QUEUE_KEY 947270
+#define QUEUE_KEY 1234567
 
 typedef struct {
-    long type;
-    int command;
+    long type;   // Typ wiadomości
+    int command; // Polecenie od pracownika technicznego (2 = wpuszczanie, 3 = opuszczanie)
+    int team_id; // ID drużyny kibica
 } Message;
 
 int main() {
+    // Otwieranie kolejki komunikatów
     int msgid = msgget(QUEUE_KEY, 0666);
     if (msgid == -1) {
         perror("Blad otwierania kolejki komunikatow");
         exit(1);
     }
 
+    // Losowanie drużyny kibica (1 lub 2)
+    srand(getpid());
+    int my_team = rand() % 2 + 1;
+
+    // Wysłanie wiadomości do pracownika z prośbą o wejście
     Message msg;
+    msg.type = 2;            // Typ wiadomości dla pracownika
+    msg.command = 0;         // Brak komendy (inicjalizacja)
+    msg.team_id = my_team;   // Drużyna kibica
+
+    if (msgsnd(msgid, &msg, sizeof(msg) - sizeof(long), 0) == -1) {
+        perror("Blad wysylania wiadomosci do pracownika");
+        exit(1);
+    }
+
+    printf("Kibic z drużyny %d czeka na wejście na stadion.\n", my_team);
+
+    // Kibic nasłuchuje poleceń od pracownika w nieskończonej pętli
     while (1) {
-        // Odbieranie sygnalu od pracownika (type = 2)
-        if (msgrcv(msgid, &msg, sizeof(msg.command), 2, 0) == -1) {
-            perror("Blad odbioru wiadomosci w kibicu");
+        if (msgrcv(msgid, &msg, sizeof(msg) - sizeof(long), 2, 0) == -1) {
+            perror("Blad odbioru wiadomosci od pracownika");
             exit(1);
         }
 
+        // Reakcja na sygnały od pracownika
         if (msg.command == 2) {
-            printf("Kibic: Wchodze na stadion.\n");
+            printf("Kibic z drużyny %d wchodzi na stadion.\n", my_team);
         } else if (msg.command == 3) {
-            printf("Kibic: Opuszczam stadion.\n");
-            break; // Kibic konczy dzialanie po sygnale 3
+            printf("Kibic z drużyny %d opuszcza stadion.\n", my_team);
+            break; // Opuszczenie stadionu kończy działanie kibica
         }
     }
 
